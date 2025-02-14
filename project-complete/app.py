24989539831f72ecb6e767e5b7dccb931dfeb3c3
marcusgarim <marcusgarim@gmail.com>
2025-02-14 14:06:55 -0300

    updade_02.14

--- project-complete/app.py
+++ project-complete/app.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/') # Define a rota
+def hello_world(): # Define uma rota raíz (página inicial) e a função que será executada
+    return 'Hello, World!'
+
+if __name__ == "__main__": # Verifica se o script está sendo executado diretamente
+    app.run(debug=True)
